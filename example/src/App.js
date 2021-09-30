63587795263cd11bc459fe7eae45240a31602c9a
Lucas <lucas.ersson@student.kyh.se>
2021-09-30 16:48:43 +0200

    fixed styling

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { useMousePosition } from 'use-mouseposition'
-import './app.css'
 
 const App = () => {
   const doc = document.getElementById('exampleEl')
   const example = useMousePosition(doc)
   const {x, y} = example
+
+  const style = {
+    width: '200px',
+    height: '200px',
+    border: '2px solid black'
+
+  }
+
   return (
     <div>
       {`X: ${x}, Y: ${y}`}
-      <div id='exampleEl' />
+      <div id='exampleEl' style={style} />
     </div>
   )
 }
